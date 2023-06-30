f5561b2c0301e3f07ea0ff7fbad0160272e18d3e
Peter Huynh <peterhyh@yahoo.com>
2023-06-30 17:24:07 -0500

    useState added

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,6 +1,8 @@
 import HomeScreen from './HomeScreen';
+import { useState } from 'react';
 
 const Main = () => {
+    const [content, setContent] = useState();
     return (
         <HomeScreen />
     )
