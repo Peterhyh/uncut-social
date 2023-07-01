92cf0117fab3e7f91e4e736e6b53a26c4466d1a3
Peter Huynh <peterhyh@yahoo.com>
2023-07-01 16:58:07 -0500

    profile screen added

---
+++ screens/ProfileScreen.js
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+import { Avatar } from 'react-native-elements';
+
+const ProfileScreen = () => {
+    return (
+        <View>
+            <Avatar />
+        </View>
+    )
+};
+
+export default ProfileScreen;
