2102d00c6287be9990f91204f5a02ac28300e66a
Peter Huynh <peterhyh@yahoo.com>
2023-07-03 16:35:33 -0500

    createStackNavigator added

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,9 +1,38 @@
 // import HomeScreen from './HomeScreen';
 import ProfileScreen from './ProfileScreen';
+import HomeScreen from './HomeScreen';
 import { useState } from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const Main = () => {
     const [content, setContent] = useState();
+
+    const DirectoryNavigator = () => {
+        const Stack = createStackNavigator();
+        return (
+            <Stack.Navigator
+                initialRouteName='Home'
+                screenOptions={{
+                    headerStyle: { backgroundColor: '#202020' },
+                    headerTintColor: '#212121',
+                }}
+            >
+                <Stack.Screen
+                    name='Home'
+                    component={HomeScreen}
+                    options={{ title: 'Home' }}
+                />
+
+                <Stack.Screen
+                    name='Profile'
+                    component={ProfileScreen}
+                    options={() => ({
+                        title: route.params.profile
+                    })}
+                />
+            </Stack.Navigator>
+        );
+    };
     return (
         <ProfileScreen />
     )
