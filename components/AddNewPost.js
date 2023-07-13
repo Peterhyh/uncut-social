dcbfc31b4754c9a96adbdfcefc2737cb043d09e2
Peter Huynh <peterhyh@yahoo.com>
2023-07-12 21:05:45 -0500

    Form Uploader update

--- components/AddNewPost.js
+++ components/AddNewPost.js
@@ -44,7 +44,7 @@ const styles = StyleSheet.create({
         color: '#fff',
         position: 'absolute',
         left: '42%',
-        fontweight: 700,
+        fontWeight: 700,
     },
 });
 
--- components/AddNewPost/FormUploader.js
+++ components/AddNewPost/FormUploader.js
@@ -1,19 +1,24 @@
 import * as Yup from 'yup';
 import { Formik } from 'formik';
 import PLACEHOLDER_IMAGE from '../../assets/images/placeholderImage.png';
-import { Image, StyleSheet, View, TextInput } from 'react-native';
+import { Image, StyleSheet, View, TextInput, Text, Button } from 'react-native';
+import { Divider } from 'react-native-elements';
+import { useState } from 'react';
 
-const schema = Yup.object().shape({
+const formSchema = Yup.object().shape({
     imageUrl: Yup.string().url().required('A URL is required'),
-    caption: Yup.string().max(2200, 'Caption has reached the character limit')
-});
+    caption: Yup.string().max(2200, 'Caption has reached the character limit'),
+})
 
 const FormUploader = () => {
+    const [thumbnailPic, setThumbnailPic] = useState(PLACEHOLDER_IMAGE);
+
     return (
         <Formik
             initialValues={{ imageUrl: '', caption: '' }}
-            validationSchema={schema}
+            validationSchema={formSchema}
             onSubmit={(values) => console.log(values)}
+            validateOnMount={true}
         >
             {({
                 handleBlur,
@@ -21,23 +26,41 @@ const FormUploader = () => {
                 handleSubmit,
                 values,
                 errors,
-                isInvalid,
+                isValid,
             }) => (
                 <>
                     <View style={styles.container}>
                         <Image
                             style={styles.uploadedImage}
-                            source={PLACEHOLDER_IMAGE}
+                            source={thumbnailPic}
                         />
-                        <TextInput
-                            placeholder='Write a caption...'
-                            placeholderTextColor='gray'
-                        />
+                        <View style={styles.captionContainer}>
+                            <TextInput
+                                style={styles.input}
+                                placeholder='Write a caption...'
+                                placeholderTextColor='gray'
+                                multiline={true}
+                                onChangeText={handleChange('caption')}
+                                onBlur={handleBlur('caption')}
+                                value={values.caption}
+                            />
+                        </View>
                     </View>
+                    <Divider width={0.2} orientation='vertical' />
                     <TextInput
+                        style={styles.input}
                         placeholder='Enter image Url'
                         placeholderTextColor='gray'
+                        onChangeText={handleChange('imageUrl')}
+                        onBlur={handleBlur('imageUrl')}
+                        value={values.imageUrl}
                     />
+                    {errors.imageUrl && (
+                        <Text style={styles.imageUrlErrorMessage}>
+                            {errors.imageUrl}
+                        </Text>
+                    )}
+                    <Button onPress={handleSubmit} disabled={!isValid} title='share' />
                 </>
             )}
         </Formik>
@@ -54,6 +77,19 @@ const styles = StyleSheet.create({
     uploadedImage: {
         height: 100,
         width: 100,
+        borderWidth: 2,
+        borderColor: 'red',
+    },
+    captionContainer: {
+        flex: 1,
+        marginLeft: 12,
+        marginTop: 10,
+    },
+    imageUrlErrorMessage: {
+        color: 'red',
+    },
+    input: {
+        color: '#fff',
     },
 });
 
